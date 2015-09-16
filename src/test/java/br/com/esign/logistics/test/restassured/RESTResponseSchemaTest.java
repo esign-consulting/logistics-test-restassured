2d29a63482c199d9df834aba5747cd6d9c6db864
Gustavo Muniz do Carmo <gustavo@esign.com.br>
2015-09-15 22:51:55 -0300

    JSON Schema Validation

--- src/test/java/br/com/esign/logistics/test/restassured/RESTResponseSchemaTest.java
+++ src/test/java/br/com/esign/logistics/test/restassured/RESTResponseSchemaTest.java
@@ -60,4 +60,148 @@ public class RESTResponseSchemaTest {
                 .body(matchesJsonSchemaInClasspath("map-response-schema.json"));
     }
     
+    /**
+     * Checks map existence.
+     */
+    @Test
+    public void testB() {
+        RestAssured
+            .when()
+                .get()
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("map-collection-response-schema.json"));
+    }
+    
+    /**
+     * Checks the unique map constraint.
+     */
+    @Test
+    public void testC() {
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"name\": \"REST-assured JSON Schema Test\"}")
+            .when()
+                .post()
+            .then()
+                .statusCode(500)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("bad-response-schema.json"));
+    }
+    
+    /**
+     * Creates routes for the map.
+     */
+    @Test
+    public void testD() {
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"origin\": {\"name\": \"A\"}, \"destination\": {\"name\": \"B\"}, \"distance\": 10}")
+                .pathParam("slug", "rest-assured-json-schema-test")
+            .when()
+                .post("/{slug}/routes")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("route-response-schema.json"));
+        
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"origin\": {\"name\": \"B\"}, \"destination\": {\"name\": \"D\"}, \"distance\": 15}")
+                .pathParam("slug", "rest-assured-json-schema-test")
+            .when()
+                .post("/{slug}/routes")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("route-response-schema.json"));
+        
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"origin\": {\"name\": \"A\"}, \"destination\": {\"name\": \"C\"}, \"distance\": 20}")
+                .pathParam("slug", "rest-assured-json-schema-test")
+            .when()
+                .post("/{slug}/routes")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("route-response-schema.json"));
+    }
+    
+    /**
+     * Checks the unique route constaint.
+     */
+    @Test
+    public void testE() {
+        RestAssured
+            .given()
+                .contentType(ContentType.JSON)
+                .body("{\"origin\": {\"name\": \"A\"}, \"destination\": {\"name\": \"C\"}, \"distance\": 20}")
+                .pathParam("slug", "rest-assured-json-schema-test")
+            .when()
+                .post("/{slug}/routes")
+            .then()
+                .statusCode(500)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("bad-response-schema.json"));
+    }
+    
+    /**
+     * Removes last route.
+     */
+    @Test
+    public void testF() {
+        RestAssured
+            .given()
+                .pathParam("mapSlug", "rest-assured-json-schema-test")
+                .pathParam("routeSlug", "a-c")
+            .when()
+                .delete("/{mapSlug}/routes/{routeSlug}")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("response-schema.json"));
+    }
+    
+    /**
+     * Tests best route functionality.
+     */
+    @Test
+    public void testG() {
+        RestAssured
+            .given()
+                .pathParam("mapSlug", "rest-assured-json-schema-test")
+                .queryParam("originName", "A")
+                .queryParam("destinationName", "D")
+                .queryParam("autonomy", 10)
+                .queryParam("gasPrice", 2.50)
+            .when()
+                .get("/{mapSlug}/bestRoute")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("best-route-response-schema.json"));
+    }
+    
+    /**
+     * Removes the map.
+     */
+    @Test
+    public void testH() {
+        RestAssured
+            .given()
+                .pathParam("slug", "rest-assured-json-schema-test")
+            .when()
+                .delete("/{slug}")
+            .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body(matchesJsonSchemaInClasspath("response-schema.json"));
+    }
+    
 }
